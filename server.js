1060c2a27e994349f6bc9ff0c9e6e22d3743eb41
Josh Wedekind <josh@halfnibble.com>
2024-02-26 18:48:12 -0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+const PORT = process.env.PORT;
+
+app.get('/', (req, res) => {
+    res.send('Hello Mohammad Zuiter!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+module.exports = app;
